058b7e8e817ff74b52c1a4875421e5790f0b3cdc
vaishali312003 <162013691+vaishali312003@users.noreply.github.com>
2024-04-21 01:29:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
+import subprocess
+
+# Install OpenCV package
+subprocess.call(["pip", "install", "opencv-python-headless"])
+
+# Now import OpenCV
 import cv2
 import streamlit as st
 import numpy as np
 
-# Custom class to maintain session state
-class SessionState:
-    def __init__(self, **kwargs):
-        self.__dict__.update(**kwargs)
-
 def main():
     st.title("Video Capture with OpenCV")
-
-    # Create a session state object
-    session_state = SessionState(stop_button_pressed=False)
 
     cap = cv2.VideoCapture(0)
 
@@ -22,7 +20,7 @@ def main():
 
     frame_placeholder = st.empty()
 
-    while cap.isOpened() and not session_state.stop_button_pressed:
+    while cap.isOpened():
         ret, frame = cap.read()
 
         if not ret:
